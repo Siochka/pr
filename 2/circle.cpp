32038a7aa2e06a1d744fb714a3ce2797ceb2cc3f
Siochka <125833195+Siochka@users.noreply.github.com>
2023-02-20 01:08:48 +0300

    Update circle.cpp

--- 2/circle.cpp
+++ 2/circle.cpp
@@ -4,26 +4,26 @@
 
 using namespace std;
 
-void set_circle(float r, float x, float y) {
+void Circle::set_circle(float r, float x, float y) {
 	radius = r;
 	x_center = x;
 	y_center = y;
 }
-float square() {
+float Circle::square() {
 	float s = 3.14 * radius * radius;
 	return s;
 }
-bool triangle_around(float a, float b, float c) {
+bool Circle::triangle_around(float a, float b, float c) {
 	double p = (a + b + c) / 2;
 	double S = sqrt(p * (p - a) * (p - b) * (p - c));
 	return (radius >= ((a + b + c) / (4 * S)));
 }
-bool triangle_in(float a, float b, float c) {
+bool Circle::triangle_in(float a, float b, float c) {
 	double p = (a + b + c) / 2;
 	double S = sqrt(p * (p - a) * (p - b) * (p - c));
 	return (radius <= S / p);
 }
-bool check_circle(float r, float x_cntr, float y_cntr) {
+bool Circle::check_circle(float r, float x_cntr, float y_cntr) {
 	float d = sqrt(pow((x_cntr - x_center), 2) + pow((y_cntr - y_center), 2));
 	return (radius + r <= d);
 }
