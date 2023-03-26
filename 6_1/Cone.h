2cb9903e2f006b39b31b91b25b97a76aa9732fd1
Siochka <125833195+Siochka@users.noreply.github.com>
2023-03-26 22:02:23 +0300

    Add files via upload

---
+++ 6_1/Cone.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+const double pi = 3.14159265;
+
+class Cone {
+    double x, y, z;
+    double radius;
+    double height;
+public:
+    Cone();
+    Cone(double r, double h);
+    Cone(double a, double b, double c, double r, double h);
+
+    void setCoordinate(double a, double b, double c);
+    void setRadius(double r);
+    void setHeight(double h);
+    double getRadius();
+    double getHeight();
+
+    double area();
+    double volume();
+
+    friend ostream& operator<<(ostream& stream, Cone obj);
+};
+
+// Вывод данных конуса
+ostream& operator<<(ostream& stream, Cone obj);
